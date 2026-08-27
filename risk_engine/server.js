ee163a37044f5169415d24fc46679c1085319acb
Shreyas <svs001907@gmail.com>
2026-08-28 00:14:58 +0530

    Connect API to risk engine

--- risk_engine/server.js
+++ risk_engine/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { calculateRiskResponse } = require("./index");
 
 const app = express();
 
@@ -8,6 +9,24 @@ app.get("/", function (req, res) {
   res.json({
     message: "Food Chain Guardian Risk Engine API is running"
   });
+});
+
+app.post("/risk", function (req, res) {
+  try {
+    const { foodType, sensorData, riskResult } = req.body;
+
+    const result = calculateRiskResponse(
+      foodType,
+      sensorData,
+      riskResult
+    );
+
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    });
+  }
 });
 
 const PORT = 3000;
